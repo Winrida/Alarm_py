ae71607c92b9b3bdba62105f8e61bd61ef464201
Winrida <winrida@yandex.ru>
2021-10-21 11:21:35 +0300

    Added ringtones.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from playsound import *
 from datetime import *
+import pyglet
+from threading import Thread
 
 
 def validate_time(alarm_time):
@@ -16,13 +17,33 @@ def validate_time(alarm_time):
             return 'Отлично!'
 
 
+def validate_melody(melody):
+    if melody < 1 or melody > 4:
+        return 'Выберете мелодию из списка.'
+    else:
+        return 'Отлично!'
+
+
+def audio(files):
+    media = pyglet.media.load(files)
+    media.play()
+    pyglet.app.run()
+
+
 while True:
     alarm_time = input('Введите время в следующем формате: \'HH:MM:SS\' \nВремя будильника: ')
     validate = validate_time(alarm_time)
     if validate != 'Отлично!':
         print(validate)
     else:
-        print(f"Будильник установлен на время: {alarm_time}...")
+        melody = int(input('Выберете номер мелодии: \n1. Лунная Соната - Бетховен\n2. Avicii - Wake Me Up\n3. '
+                           'Illenium ft. '
+                           'Annika Wells - Crawl outta love \n4. Little Richard - Long Tall Sally\nНомер мелодии: '))
+        validate_m = validate_melody(melody)
+        if validate_m != 'Отлично!':
+            print(validate_m)
+        else:
+            print(f"Будильник установлен на время: {alarm_time}...")
         break
 
 alarm_hour = int(alarm_time[0:2])
@@ -40,4 +61,16 @@ while True:
         if alarm_min == current_min:
             if alarm_sec == current_sec:
                 print('Просыпайся!')
+                if melody == 1:
+                    player = Thread(target=audio, args=("Лунная Соната - Бетховен.mp3",))
+                    player.start()
+                elif melody == 2:
+                    player = Thread(target=audio, args=("Avicii - Wake Me Up.mp3",))
+                    player.start()
+                elif melody == 3:
+                    player = Thread(target=audio, args=("Illenium ft. Annika Wells - Crawl outta love.mp3",))
+                    player.start()
+                elif melody == 4:
+                    player = Thread(target=audio, args=("Little Richard - Long Tall Sally.mp3",))
+                    player.start()
                 break
